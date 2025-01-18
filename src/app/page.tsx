e242ef3bc96f0a0925fefb866a40fcb340e6c5c4
Ai-sha19 <aisha.aysha216@gmail.com>
2025-01-19 00:38:26 +0500

    show products on UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,80 @@
-import Features from "./components/features";
-import GetInTouch from "./components/get-in-touch";
-import Hero from "./components/hero";
-import NewCaramics from "./components/new-caramics";
-import PopularProduct from "./components/popular-products";
-import SignupSection from "./components/sign-up-section";
+import Image from "next/image";
+import { sanityfetch } from "@/sanity/lib/fetch"
+import { sanityFetch } from "@/sanity/lib/live";
+import { allproducts } from "@/sanity/lib/queries";
+import { product } from "@/sanity/schemaTypes/product";
 
-
-// src/app/page.tsx
+type Product={
+  _id:string;
+  name:string;
+  description:string;
+  price:number;
+  imageurl:string
+}
 export default async function Home() {
+  const products: Product[] = await sanityfetch({query:allproducts })
+  
+  return(
+    <div>
+      <h1>
+        Products
+      </h1>
+      <div className="grid grid-cols-3 gap-4">
+{
+  products.map((product)=>(
+<div className="border p-4 rounded-lg flex flex-col items-center" key={product._id}>
+<Image src={product.imageurl} alt={product.name} className="w-60" width={500} height={500}/> 
+<h2 className="text-xl font-bold text-center">
+  {product.name}
+ </h2>
+ <p className="text-center">
+  {product.description}
+</p>
+<p className="text-center">
+  {product.price}
+</p>
+  </div>
+  ))
+}
+      </div>
+    </div>
+  )
+}
 
 
 
-  return (
- <>
- <Hero />
- <Features />
- <NewCaramics />
- <PopularProduct />
- <SignupSection />
- <GetInTouch />
+
+
+
+
+
+
+
+
+
+// import Features from "./components/features";
+// import GetInTouch from "./components/get-in-touch";
+// import Hero from "./components/hero";
+// import NewCaramics from "./components/new-caramics";
+// import PopularProduct from "./components/popular-products";
+// import SignupSection from "./components/sign-up-section";
+
+
+// // src/app/page.tsx
+// export default async function Home() {
+
+
+
+//   return (
+//  <>
+//  <Hero />
+//  <Features />
+//  <NewCaramics />
+//  <PopularProduct />
+//  <SignupSection />
+//  <GetInTouch />
  
 
- </>
-  );
-}
+//  </>
+//   );
+// }
